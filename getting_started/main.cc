9fa120e24761a907dff1b07d5e0f9d9420cfa375
FKNoble <f.k.noble@massey.ac.nz>
2017-03-22 14:44:18 +1300

    Updated the projuect file and commented code.

--- getting_started/main.cc
+++ getting_started/main.cc
@@ -6,31 +6,59 @@
 //
 //THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+/*!
+* \file main.cc
+* \author Dr. Frazer K. Noble
+* \date 2017
+* \brief This program demonstrates how the Open Computer Vision (OpenCV) library can be used.
+*
+* \section Description.
+*
+* \note Comments are written in Doxygen-compliant format (see \link https://www.stack.nl/~dimitri/doxygen/manual/docblocks.html \endlink). Here, I have used Qt style (/ *! ...text... * /)
+*/
+
 #include "main.h"
 
+DEFINE_bool(d, false, "[true/false]");
+
+/*
+* \brief Main entry point for the program.
+* \param argc is the number of command line parameters provided.
+* \param argv is the string of command line parameters provided.
+* \return Returns 0 on success; -1 on failure.
+*/
 int main(int argc, char* argv[]) {
 
-	cv::VideoCapture camera(0);
+	gflags::ParseCommandLineFlags(&argc, &argv, true); //! Use gflags to parse the command line.
 
-	cv::namedWindow("Hello World", cv::WINDOW_AUTOSIZE);
+	bool debug = false;
+
+	if (FLAGS_d) { //! If --d is set in the command line, set debug.
+		debug = true;
+	}
+
+	cv::VideoCapture camera(0); //! Creates a cv::VideoCapture object, which opens the default video capture device, e.g. a USB web camera.
+
+	cv::namedWindow("Hello World", cv::WINDOW_AUTOSIZE); //! Creates a cv::namedWindow, which is used to display images in.
 
 	while (true) {
 
-		cv::Mat image;
+		cv::Mat image; //! Create a cv::Mat object to hold an image.
 
-		camera >> image;
+		camera >> image; //! Get an image from camera.
 
-		if (!image.empty()) {
+		if (!image.empty()) { //! If the image is non-zero, then display it in the window.
 			cv::imshow("Hello World", image);
 		}
 
-		char c = cv::waitKey(1);
+		char c = cv::waitKey(1); //! Wait 1 ms to see if the 'Esc' key is pressed.
 		if (c == 27) {
 			break;
 		}
 	}
 
-	cv::destroyAllWindows();
+	//! Close program; release resources.
+	cv::destroyAllWindows(); 
 	camera.release();
 
 	return 0;
